4588ae31c42067f5b6f0adc7edb3af375932603e
zellerjan <jan.zeller@hotmail.ch>
2024-10-25 21:17:31 +0200

    updatet content

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
 feather.replace();
+
+// Prüft, ob ein Element in den Sichtbereich kommt
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("fade-in");
+        }
+    });
+}, { threshold: 0.1 });
+
+// Ziel-Elemente auswählen und dem Observer hinzufügen
+document.querySelectorAll("section").forEach((section) => {
+    observer.observe(section);
+});
